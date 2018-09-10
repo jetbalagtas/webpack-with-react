ef00daa9b06180dbb52780959ad10748dbba4a2f
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-10 19:02:59 -0400

    create basic react application

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render () {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Awesome Pizza Lovers!</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
