21f851558760923ce29cdc8f345c5d6cd668abcd
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-10 20:03:16 -0400

    add css file support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -17,6 +18,37 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                 autoprefixer({
+                   browsers: [
+                    "> 1%",
+                    "last 2 versions"
+                   ]
+                 })
+              ]
+            }
+          }
+        ]
       }
     ]
   }
